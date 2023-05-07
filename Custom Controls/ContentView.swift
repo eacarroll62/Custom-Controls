d929fdd26a7fe53b7559b739c39f3fd650edbb09
Eric A Carroll <peacarroll@comcast.net>
2023-05-07 14:48:31 -0400

    Initial Commit

---
+++ Custom Controls/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Custom Controls
+//
+//  Created by Eric Carroll on 5/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custom Controls/Custom_ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_ControlsApp.swift
+//  Custom Controls
+//
+//  Created by Eric Carroll on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Custom_ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
